6c84e915191f7af67083feff22d56032c98e169f
RayFlaherty <90649331+RayFlaherty@users.noreply.github.com>
2021-12-07 17:13:51 -0600

    debugging script

--- Develop/script.js
+++ Develop/script.js
@@ -3,7 +3,7 @@ const upperCaseChar="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerCaseChar= "abcdefghijklmnopqrstuvwxyz";
 const symbolsChar = "`~!@#$%^&*()_-+={[}]|\;:'/?.>,<";
 const numbersChar = "1234567890";
-const length = document.getElementById("length");
+const len = document.querySelector("#length");
 const upperCase= document.getElementById("uppercase");
 const lowerCase= document.getElementById("lowercase");
 const symbols = document.getElementById("symbols");
@@ -18,32 +18,22 @@ const numbers = document.getElementById("numbers");
     lowerCase.checked ? (characters += lowerCaseChar) : "";
     symbols.checked ? (characters += symbolsChar) : "";
     numbers.checked ? (characters += numbersChar) : "";
-   passwordText.value = generatePassword(length.value, characters);
- //for (var i = 0; i <= length; i++) {
-   //var randomNumber = Math.floor(Math.random() * chars.length);
-  // password += chars.substring(randomNumber, randomNumber +1);
- 
-  //return characters;
- const generatePassword =(length,characters) => {
-    var password = "";
-    for (let i = 0; i < length; i++) {
-      password += characters.charAt(
-        Math.floor(Math.random() * characters.length)
-
-        
-      );
-    }
-  }
-  };
-  
-  
-
    
-  // return password;    
- //}
- 
+    var passwordLength = len.value;
+    var chars=characters; 
+    console.log(passwordLength);
+    console.log(chars.length);
+    for (var i = 0; i < passwordLength; i++) {
+      var randomNumber = Math.floor(Math.random() * chars.length);
+      password += chars.substring(randomNumber, randomNumber +1);
+        
+    };
+    console.log(password);
+    return password; 
+  };
 
-document.getElementById("password").value = password;
+  
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
