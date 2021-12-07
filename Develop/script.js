ceead0577044cd5d9e884bdb4690685551cbd430
RayFlaherty <90649331+RayFlaherty@users.noreply.github.com>
2021-12-06 22:20:17 -0600

    debugged

--- Develop/script.js
+++ Develop/script.js
@@ -11,19 +11,21 @@
    var randomNumber = Math.floor(Math.random() * chars.length);
    password += chars.substring(randomNumber, randomNumber +1);
   }
-       
+   return password;    
  }
+ 
 
 document.getElementById("password").value = password;
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate")
+var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
+
 
 }
 
